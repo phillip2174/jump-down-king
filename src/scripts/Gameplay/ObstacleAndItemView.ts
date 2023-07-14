175f21b6034639deea596cabd2d87b35f1e7bc5b
phillip2174 <puhzang_l@sci.tu.ac.th>
2023-07-14 15:38:04 +0700

    upload-newest-build
    Upload Newest Build

--- src/scripts/Gameplay/ObstacleAndItemView.ts
+++ src/scripts/Gameplay/ObstacleAndItemView.ts
@@ -9,193 +9,201 @@ import { PodProvider } from '../pod/PodProvider'
 import { ObstacleType } from './ObstacleType'
 import { ObstacleMoveType } from './ObstacleMoveType'
 import { GameplayUIState } from './GameplayUIState'
+import { PoolManager } from '../plugins/pool/PoolManager'
 
 export default class ObstacleAndItemView extends GameObjects.GameObject {
-   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
 
-   private bodyChecker: GameObjects.Rectangle
-   private obstacleTriggerMove: GameObjects.Rectangle
+    private bodyChecker: GameObjects.Rectangle
+    private obstacleTriggerMove: GameObjects.Rectangle
 
-   private playerObject: PlayerObjectView
+    private playerObject: PlayerObjectView
 
-   private timerObstacleLeftSubscription: Subscription = new Subscription()
-   private timerObstacleRightSubscription: Subscription = new Subscription()
-   private coinSubscription: Subscription = new Subscription()
-   private phaseSubscription: Subscription = new Subscription()
+    private timerObstacleLeftSubscription: Subscription = new Subscription()
+    private timerObstacleRightSubscription: Subscription = new Subscription()
+    private coinSubscription: Subscription = new Subscription()
+    private phaseSubscription: Subscription = new Subscription()
 
-   private gamePlayPod: GameplayPod
-   private tempCurrentLevel: number
+    private gamePlayPod: GameplayPod
+    private tempCurrentLevel: number
 
-   constructor(scene: Phaser.Scene) {
-      super(scene, 'gameObject')
-   }
+    private pool: PoolManager
+    private group: Phaser.GameObjects.Group
 
-   public doInit(playerObject: PlayerObjectView) {
-      console.log('Init ObstacleView')
+    constructor(scene: Phaser.Scene) {
+        super(scene, 'gameObject')
+    }
 
-      this.gamePlayPod = PodProvider.instance.gamePlayPod
+    public doInit(playerObject: PlayerObjectView) {
+        console.log('Init ObstacleView')
 
-      this.playerObject = playerObject
-      this.cursors = this.scene.input.keyboard.createCursorKeys()
+        this.gamePlayPod = PodProvider.instance.gamePlayPod
 
-      this.bodyChecker = this.scene.add
-         .rectangle(this.scene.cameras.main.centerX, -30, this.scene.cameras.main.width * 1.5, 50, 0xffffff)
-         .setDepth(4)
-         .setAlpha(0)
+        this.playerObject = playerObject
+        this.cursors = this.scene.input.keyboard.createCursorKeys()
 
-      this.scene.physics.add.existing(this.bodyChecker)
+        this.bodyChecker = this.scene.add
+            .rectangle(this.scene.cameras.main.centerX, -30, this.scene.cameras.main.width * 1.5, 50, 0xffffff)
+            .setDepth(4)
+            .setAlpha(0)
 
-      this.obstacleTriggerMove = this.scene.add
-         .rectangle(
-            this.scene.cameras.main.centerX,
-            this.scene.cameras.main.centerY + 50,
-            this.scene.cameras.main.width * 1.5,
-            1,
-            0xffffff
-         )
-         .setDepth(4)
-         .setAlpha(0)
+        this.scene.physics.add.existing(this.bodyChecker)
 
-      this.scene.physics.add.existing(this.obstacleTriggerMove)
+        this.obstacleTriggerMove = this.scene.add
+            .rectangle(
+                this.scene.cameras.main.centerX,
+                this.scene.cameras.main.centerY + 50,
+                this.scene.cameras.main.width * 1.5,
+                1,
+                0xffffff
+            )
+            .setDepth(4)
+            .setAlpha(0)
 
-      //this.obstacleChecker.body
+        this.scene.physics.add.existing(this.obstacleTriggerMove)
 
-      //@ts-ignore
-      //this.obstacleChecker.body.setCollideWorldBounds(true);
+        this.pool = PoolManager.instance
+        this.pool.doInit(this.scene)
+        this.pool.createPooler('ObstacleCellView', {
+            classType: ObstacleCellView,
+            maxSize: 10,
+            createCallback: (object: any) => {
+                this.onCreate(object)
+            },
+        })
+        this.group = this.pool.getPoolerGroupWithName('ObstacleCellView')
 
-      //let bodyPhysics: Physics.Arcade.Body = this.bodyChecker.body as Physics.Arcade.Body
-      //    //@ts-ignore
-      // bodyPhysics.setImmovable(true)
-      // bodyPhysics.setSize(400, 160, false)
-      // bodyPhysics.setOffset(0, 150)
+        //this.obstacleChecker.body
 
-      this.phaseSubscription = this.gamePlayPod.phase.subscribe((phase) => {
-         if (PodProvider.instance.gamePlayUIPod.currentGameplayUIState.value == GameplayUIState.Gameplay) {
-            this.timerObstacleLeftSubscription?.unsubscribe()
-            this.timerObstacleRightSubscription?.unsubscribe()
+        //@ts-ignore
+        //this.obstacleChecker.body.setCollideWorldBounds(true);
 
-            this.tempCurrentLevel = phase
+        //let bodyPhysics: Physics.Arcade.Body = this.bodyChecker.body as Physics.Arcade.Body
+        //    //@ts-ignore
+        // bodyPhysics.setImmovable(true)
+        // bodyPhysics.setSize(400, 160, false)
+        // bodyPhysics.setOffset(0, 150)
 
-            if (!this.gamePlayPod.currentGameConfig.isSpawnObstacleMoveBoth) {
-               let laneRandom = this.randomInt(1, 2)
-               if (laneRandom == 1) {
-                  this.CommandSpawnObstacleLane(true, false)
-               } else {
-                  this.CommandSpawnObstacleLane(false, true)
-               }
-            } else {
-               this.CommandSpawnObstacleLane(true, true)
+        this.phaseSubscription = this.gamePlayPod.phase.subscribe((phase) => {
+            if (PodProvider.instance.gamePlayUIPod.currentGameplayUIState.value == GameplayUIState.Gameplay) {
+                this.timerObstacleLeftSubscription?.unsubscribe()
+                this.timerObstacleRightSubscription?.unsubscribe()
+
+                this.tempCurrentLevel = phase
+
+                if (!this.gamePlayPod.currentGameConfig.isSpawnObstacleMoveBoth) {
+                    let laneRandom = this.randomInt(1, 2)
+                    if (laneRandom == 1) {
+                        this.CommandSpawnObstacleLane(true, false)
+                    } else {
+                        this.CommandSpawnObstacleLane(false, true)
+                    }
+                } else {
+                    this.CommandSpawnObstacleLane(true, true)
+                }
+                this.SpawnCoin()
             }
-            this.SpawnCoin()
-         }
-      })
-   }
+        })
+    }
 
-   private CommandSpawnObstacleLane(isSpawnMoveObstacleFirstLane: boolean, isSpawnMoveObstacleSecondLane: boolean) {
-      this.SpawnObstacleLane(
-         this.timerObstacleLeftSubscription,
-         25,
-         ObstacleMoveType.LeftToRight,
-         isSpawnMoveObstacleFirstLane
-      )
-      this.SpawnObstacleLane(
-         this.timerObstacleRightSubscription,
-         this.scene.cameras.main.width - 25,
-         ObstacleMoveType.RightToLeft,
-         isSpawnMoveObstacleSecondLane
-      )
-   }
+    private onCreate(obstacleCellView: ObstacleCellView) {
+        obstacleCellView.onCreate(this.playerObject, this.bodyChecker, this.obstacleTriggerMove)
+    }
 
-   public SpawnObstacleLane(
-      subscription: Subscription,
-      xPosition: number,
-      obstacleMoveType: ObstacleMoveType,
-      isSpawnMoveObstacle: boolean
-   ) {
-      subscription?.unsubscribe()
-      let minTimeObstacleTimeRand = this.gamePlayPod.currentGameConfig.minTimeObstacleTimeRand
-      let maxTimeObstacleTimeRand = this.gamePlayPod.currentGameConfig.maxTimeObstacleTimeRand
+    private CommandSpawnObstacleLane(isSpawnMoveObstacleFirstLane: boolean, isSpawnMoveObstacleSecondLane: boolean) {
+        this.SpawnObstacleLane(
+            this.timerObstacleLeftSubscription,
+            25,
+            ObstacleMoveType.LeftToRight,
+            isSpawnMoveObstacleFirstLane
+        )
+        this.SpawnObstacleLane(
+            this.timerObstacleRightSubscription,
+            this.scene.cameras.main.width - 25,
+            ObstacleMoveType.RightToLeft,
+            isSpawnMoveObstacleSecondLane
+        )
+    }
 
-      //let isSpawnObstacleMoveBoth = this.gamePlayPod.currentGameConfig.isSpawnObstacleMoveBoth
+    public SpawnObstacleLane(
+        subscription: Subscription,
+        xPosition: number,
+        obstacleMoveType: ObstacleMoveType,
+        isSpawnMoveObstacle: boolean
+    ) {
+        subscription?.unsubscribe()
+        let minTimeObstacleTimeRand = this.gamePlayPod.currentGameConfig.minTimeObstacleTimeRand
+        let maxTimeObstacleTimeRand = this.gamePlayPod.currentGameConfig.maxTimeObstacleTimeRand
+        //let isSpawnObstacleMoveBoth = this.gamePlayPod.currentGameConfig.isSpawnObstacleMoveBoth
+        let numberRandom = this.randomInt(minTimeObstacleTimeRand, maxTimeObstacleTimeRand)
+        let obstacleType: ObstacleType
+        if (isSpawnMoveObstacle) {
+            obstacleType =
+                ObstacleType[
+                    ObstacleType[
+                        Math.floor(Math.random() * this.gamePlayPod.currentGameConfig.obstacleIndexArray.length)
+                    ]
+                ]
+        } else {
+            obstacleType = ObstacleType.ObstacleStatic
+        }
+        // const randObstacleType = this.randomEnum(ObstacleType) as ObstacleType
+        // console.log(obstacleMoveType + ' : ' + ObstacleType[randObstacleType])
+        let currentPhase = this.gamePlayPod.currentGameConfig.phase
+        subscription = timer(numberRandom * 1000).subscribe((_) => {
+            if (PodProvider.instance.gamePlayUIPod.currentGameplayUIState.value == GameplayUIState.GameResult) return
+            if (this.tempCurrentLevel == currentPhase) {
+                console.log(this.group.getLength())
+                let obstacle: ObstacleCellView = this.group.get()
+                obstacle.init(xPosition, this.scene.cameras.main.height, obstacleType, obstacleMoveType)
 
-      let numberRandom = this.randomInt(minTimeObstacleTimeRand, maxTimeObstacleTimeRand)
+                this.SpawnObstacleLane(subscription, xPosition, obstacleMoveType, isSpawnMoveObstacle)
+            } else {
+                subscription?.unsubscribe()
+            }
+        })
+    }
 
-      let obstacleType: ObstacleType
-      if (isSpawnMoveObstacle) {
-         obstacleType =
-            ObstacleType[
-               ObstacleType[Math.floor(Math.random() * this.gamePlayPod.currentGameConfig.obstacleIndexArray.length)]
-            ]
-      } else {
-         obstacleType = ObstacleType.ObstacleStatic
-      }
+    public SpawnCoin() {
+        this.coinSubscription?.unsubscribe()
 
-      // const randObstacleType = this.randomEnum(ObstacleType) as ObstacleType
-      // console.log(obstacleMoveType + ' : ' + ObstacleType[randObstacleType])
-      let currentPhase = this.gamePlayPod.currentGameConfig.phase
-      subscription = timer(numberRandom * 1000).subscribe((_) => {
-         if (PodProvider.instance.gamePlayUIPod.currentGameplayUIState.value == GameplayUIState.GameResult) return
+        let minTimeCoinTimeRand = this.gamePlayPod.currentGameConfig.minTimeCoinTimeRand
+        let maxTimeCoinTimeRand = this.gamePlayPod.currentGameConfig.maxTimeCoinTimeRand
 
-         if (this.tempCurrentLevel == currentPhase) {
-            let cell = new ObstacleCellView(this.scene)
+        let numberRandom = this.randomInt(minTimeCoinTimeRand, maxTimeCoinTimeRand)
 
+        this.coinSubscription = timer(numberRandom * 1000).subscribe((_) => {
+            let cell = new CoinCellView(this.scene)
             cell.doInit(
-               this.playerObject,
-               this.bodyChecker,
-               this.obstacleTriggerMove,
-               xPosition,
-               this.scene.cameras.main.height,
-               obstacleType,
-               obstacleMoveType
+                this.playerObject,
+                this.bodyChecker,
+                this.scene.cameras.main.width / 2,
+                this.scene.cameras.main.height
             )
 
-            this.SpawnObstacleLane(subscription, xPosition, obstacleMoveType, isSpawnMoveObstacle)
-         } else {
-            subscription?.unsubscribe()
-         }
-      })
-   }
+            this.SpawnCoin()
+        })
+    }
 
-   public SpawnCoin() {
-      this.coinSubscription?.unsubscribe()
+    randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 
-      let minTimeCoinTimeRand = this.gamePlayPod.currentGameConfig.minTimeCoinTimeRand
-      let maxTimeCoinTimeRand = this.gamePlayPod.currentGameConfig.maxTimeCoinTimeRand
+    private randomEnum<T>(anEnum: T): T[keyof T] {
+        const enumValues = Object.keys(anEnum)
+            .map((n) => Number.parseInt(n))
+            .filter((n) => !Number.isNaN(n)) as unknown as T[keyof T][]
+        const randomIndex = Math.floor(Math.random() * enumValues.length)
+        const randomEnumValue = enumValues[randomIndex]
+        return randomEnumValue
+    }
 
-      let numberRandom = this.randomInt(minTimeCoinTimeRand, maxTimeCoinTimeRand)
-
-      this.coinSubscription = timer(numberRandom * 1000).subscribe((_) => {
-         let cell = new CoinCellView(this.scene)
-         cell.doInit(
-            this.playerObject,
-            this.bodyChecker,
-            this.scene.cameras.main.width / 2,
-            this.scene.cameras.main.height
-         )
-
-         this.SpawnCoin()
-      })
-   }
-
-   randomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min)
-   }
-
-   private randomEnum<T>(anEnum: T): T[keyof T] {
-      const enumValues = Object.keys(anEnum)
-         .map((n) => Number.parseInt(n))
-         .filter((n) => !Number.isNaN(n)) as unknown as T[keyof T][]
-      const randomIndex = Math.floor(Math.random() * enumValues.length)
-      const randomEnumValue = enumValues[randomIndex]
-      return randomEnumValue
-   }
-
-   public destroy(fromScene?: boolean): void {
-      //console.log(this.timerObstacleLeftSubscription)
-      this.coinSubscription?.unsubscribe()
-      this.timerObstacleLeftSubscription?.unsubscribe()
-      this.timerObstacleRightSubscription?.unsubscribe()
-      this.phaseSubscription?.unsubscribe()
-      super.destroy(fromScene)
-   }
+    public destroy(fromScene?: boolean): void {
+        //console.log(this.timerObstacleLeftSubscription)
+        this.coinSubscription?.unsubscribe()
+        this.timerObstacleLeftSubscription?.unsubscribe()
+        this.timerObstacleRightSubscription?.unsubscribe()
+        this.phaseSubscription?.unsubscribe()
+        super.destroy(fromScene)
+    }
 }
--- src/scripts/Gameplay/ObstacleCellView.ts
+++ src/scripts/Gameplay/ObstacleCellView.ts
@@ -6,8 +6,9 @@ import { GameplayPod } from '../pod/GameplayPod'
 import { PodProvider } from '../pod/PodProvider'
 import { ObstacleType } from './ObstacleType'
 import { ObstacleMoveType } from './ObstacleMoveType'
+import { IPoolObject } from '../plugins/pool/IPoolObject'
 
-export default class ObstacleCellView extends GameObjects.GameObject {
+export default class ObstacleCellView extends Physics.Arcade.Image implements IPoolObject {
     private obstacle01: GameObjects.Image
     private player: PlayerObjectView
 
@@ -19,26 +20,43 @@ export default class ObstacleCellView extends GameObjects.GameObject {
     private targetValueMove: number = 0
     private startMovePostion: number
 
-    constructor(scene: Phaser.Scene) {
-        super(scene, 'gameObject')
-    }
+    private objectChecker: GameObjects.GameObject
+    private obstacleTriggerChecker: GameObjects.GameObject
+    private yoyoTween: Phaser.Tweens.Tween
 
-    public doInit(
+    private isObjectHide: boolean
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, '')
+    }
+    public onCreate(
         playerObject: PlayerObjectView,
         objectChecker: GameObjects.GameObject,
-        obstacleTriggerChecker: GameObjects.GameObject,
-        xPosition: number,
-        yPosition: number,
-        obstacleType: ObstacleType,
-        moveType: ObstacleMoveType
+        obstacleTriggerChecker: GameObjects.GameObject
     ) {
         this.gameplayPod = PodProvider.instance.gamePlayPod
 
-        this.obstacleType = obstacleType
         this.player = playerObject
+        this.objectChecker = objectChecker
+        this.obstacleTriggerChecker = obstacleTriggerChecker
 
-        this.scene.events.addListener('update', this.onUpdate, this)
-        //this.scene.events.removeListener('update', this.onUpdate, this)
+        this.scene.physics.add.existing(this)
+
+        this.scene.physics.add.overlap(this as any, playerObject, this.onHitPlayer, null, this)
+        this.scene.physics.add.overlap(this as any, objectChecker, this.onHitChecker, null, this)
+        this.scene.physics.add.overlap(this as any, obstacleTriggerChecker, this.onObstacleTriggerDynamic, null, this)
+
+        this.setDepth(5)
+        this.setVisible(false)
+        this.setActive(false)
+    }
+
+    public init(xPosition: number, yPosition: number, obstacleType: ObstacleType, moveType: ObstacleMoveType) {
+        this.setVisible(true)
+        this.setActive(true)
+        this.isObjectHide = false
+        this.obstacleType = obstacleType
+        this.isHitObstacleTrigger = false
 
         switch (moveType) {
             case ObstacleMoveType.LeftToRight:
@@ -53,112 +71,104 @@ export default class ObstacleCellView extends GameObjects.GameObject {
 
         let randNumber = this.randomInt(1, 3)
 
+        this.setPosition(xPosition, yPosition)
         switch (this.obstacleType) {
             case ObstacleType.ObstacleStatic:
-                this.obstacle01 = this.scene.add
-                    .image(xPosition, yPosition, 'spritesPacker', 'meteorBrown_big' + randNumber + '.png')
-                    .setDepth(4)
-                    .setScale(1.2)
-                this.scene.physics.add.existing(this.obstacle01)
-
-                let bodyPhysics: Physics.Arcade.Body = this.obstacle01.body as Physics.Arcade.Body
+                this.setTexture('spritesPacker', 'meteorBrown_big' + randNumber + '.png')
+                this.setScale(1.2)
 
                 switch (randNumber) {
                     case 1:
-                        bodyPhysics.setSize(100, 60, false)
-                        bodyPhysics.setOffset(0, 10)
+                        this.body.setSize(100, 60, false)
+                        this.body.setOffset(0, 10)
                         break
                     case 2:
-                        bodyPhysics.setSize(110, 60, false)
-                        bodyPhysics.setOffset(0, 20)
+                        this.body.setSize(110, 60, false)
+                        this.body.setOffset(0, 20)
                         break
                     case 3:
                         if (moveType == ObstacleMoveType.LeftToRight) {
-                            this.obstacle01.x = this.obstacle01.x + 15
+                            this.x = this.x + 15
                         } else {
-                            this.obstacle01.x = this.obstacle01.x - 20
+                            this.x = this.x - 20
                         }
 
-                        bodyPhysics.setSize(90, 60, false)
-                        bodyPhysics.setOffset(0, 10)
+                        this.body.setSize(80, 60, false)
+                        this.body.setOffset(0, 10)
 
                         break
                 }
-
-                this.scene.physics.add.overlap(this.obstacle01 as any, objectChecker, this.onHitChecker, null, this)
-
                 break
             case ObstacleType.ObstacleDynamic:
-                this.obstacle01 = this.scene.add
-                    .image(xPosition, yPosition, 'spritesPacker', 'meteorGrey_big' + randNumber + '.png')
-                    .setDepth(4)
-                    .setScale(0.8)
-                this.scene.physics.add.existing(this.obstacle01)
+                this.setTexture('spritesPacker', 'meteorGrey_big' + randNumber + '.png')
+                this.setScale(0.8)
 
-                this.scene.physics.add.overlap(
-                    this.obstacle01 as any,
-                    obstacleTriggerChecker,
-                    this.onObstacleTriggerDynamic,
-                    null,
-                    this
-                )
+                switch (randNumber) {
+                    case 1:
+                        this.body.setSize(100, 60, false)
+                        this.body.setOffset(0, 10)
+                        break
+                    case 2:
+                        this.body.setSize(90, 60, false)
+                        this.body.setOffset(10, 20)
+                        break
+                    case 3:
+                        this.body.setSize(70, 60, false)
+                        this.body.setOffset(5, 10)
+                        break
+                }
 
                 this.scene.add.tween({
-                    targets: this.obstacle01,
+                    targets: this,
                     ease: 'cubic.inout',
                     yoyo: false,
                     repeat: 0,
                     duration: 400,
                     props: {
-                        x: { from: this.obstacle01, to: xPosition - this.startMovePostion * 1.5 },
+                        x: { from: this.x, to: xPosition - this.startMovePostion * 1.5 },
                     },
                 })
+
                 break
             case ObstacleType.ObstacleYoyo:
-                //#
-                this.obstacle01 = this.scene.add
-                    .image(xPosition, yPosition, 'spritesPacker', 'ufoRed.png')
-                    .setDepth(4)
-                    .setScale(0.8)
-                this.scene.physics.add.existing(this.obstacle01)
+                this.setTexture('spritesPacker', 'ufoRed.png')
+                this.setScale(0.8)
+                this.setSize(70, 70)
+                this.setOffset(10, 10)
 
-                this.scene.physics.add.overlap(this.obstacle01 as any, objectChecker, this.onHitChecker, null, this)
-
-                this.scene.add.tween({
-                    targets: this.obstacle01,
+                this.yoyoTween = this.scene.add.tween({
+                    targets: this,
                     ease: 'Linear',
                     yoyo: true,
                     repeat: -1,
                     duration: 1600 - -this.gameplayPod.currentGameConfig.speed,
                     props: {
-                        x: { from: this.obstacle01, to: this.targetValueMove },
+                        x: { from: this.x, to: this.targetValueMove },
                     },
                 })
-
-                break
-            default:
-                this.obstacle01 = this.scene.add
-                    .image(xPosition, yPosition, 'spritesPacker', 'meteorBrown_big' + randNumber + '.png')
-                    .setDepth(4)
-                this.scene.physics.add.existing(this.obstacle01)
-
-                this.scene.physics.add.overlap(this.obstacle01 as any, objectChecker, this.onHitChecker, null, this)
                 break
         }
 
-        this.scene.physics.add.overlap(this.obstacle01 as any, playerObject, this.onHitPlayer, null, this)
-
+        this.phaseSubscription?.unsubscribe()
         this.phaseSubscription = this.gameplayPod.phase.subscribe((phase) => {
             if (this.isHitObstacleTrigger == false) {
                 if (this.obstacleType == ObstacleType.ObstacleYoyo) {
-                    this.obstacle01.body.velocity.x = 0
-                    this.obstacle01.body.velocity.y = this.gameplayPod.currentGameConfig.speed / 2.5
+                    this.body.velocity.x = 0
+                    this.body.velocity.y = this.gameplayPod.currentGameConfig.speed / 2.5
                 } else {
-                    this.obstacle01.body.velocity.x = 0
-                    this.obstacle01.body.velocity.y = this.gameplayPod.currentGameConfig.speed
+                    this.body.velocity.x = 0
+                    this.body.velocity.y = this.gameplayPod.currentGameConfig.speed
                 }
             }
         })
+    }
+
+    hideObject(): void {
+        this.isObjectHide = true
+        this.phaseSubscription?.unsubscribe()
+        this.setPosition(this.scene.cameras.main.centerX, this.scene.cameras.main.height)
+        this.setVisible(false)
+        this.setActive(false)
     }
 
     randomInt(min, max) {
@@ -170,39 +180,53 @@ export default class ObstacleCellView extends GameObjects.GameObject {
     }
 
     onHitChecker() {
-        this.destroy()
+        if (!this.isObjectHide) {
+            this.yoyoTween?.remove()
+            this.body.velocity.x = 0
+            this.body.velocity.y = 0
+            this.hideObject()
+        }
+        //this.destroy()
     }
 
     onHitPlayer() {
-        this.player.decreaseHp()
+        if (!this.isObjectHide) {
+            this.player.decreaseHp()
+        }
         console.log('Hit obstacle')
     }
 
+    onObstacleHit() {
+        console.log('Hit self obstacle')
+    }
+
     onObstacleTriggerDynamic() {
-        if (this.isHitObstacleTrigger != true) {
-            this.isHitObstacleTrigger = true
-            // this.obstacle01.body.velocity.y = 0
-            this.scene.add.tween({
-                targets: this.obstacle01,
-                ease: 'Linear',
-                yoyo: false,
-                repeat: 0,
-                duration: 1300 - -this.gameplayPod.currentGameConfig.speed / 2,
-                props: {
-                    x: { from: this.obstacle01, to: this.targetValueMove },
-                },
-                onComplete: () => {
-                    this.destroy()
-                },
-            })
+        if (this.obstacleType == ObstacleType.ObstacleDynamic) {
+            if (this.isHitObstacleTrigger != true) {
+                this.isHitObstacleTrigger = true
+                // this.obstacle01.body.velocity.y = 0
+                this.scene.add.tween({
+                    targets: this,
+                    ease: 'Linear',
+                    yoyo: false,
+                    repeat: 0,
+                    duration: 1300 - -this.gameplayPod.currentGameConfig.speed / 2,
+                    props: {
+                        x: { from: this, to: this.targetValueMove },
+                    },
+                    onComplete: () => {
+                        this.onHitChecker()
+                    },
+                })
+            }
         }
     }
 
     destroy(formScene?: boolean) {
         if (!formScene) {
-            this.obstacle01.body.velocity.y = 0
+            // this.obstacle01.body.velocity.y = 0
             this.scene.events.removeListener('update', this.onUpdate, this)
-            this.obstacle01?.destroy()
+            //this.obstacle01?.destroy()
         }
 
         this.phaseSubscription?.unsubscribe()
